5cf3b30b01051eaf8e830c70eaa3556e6ebd8c4a
Vinicius Mussato <vinicius.mussato@gmail.com>
2018-01-25 16:40:52 -0200

    import tachyons in index.js + Added styles to navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
 	return (
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-			<p>Sign Out</p>
+			<p className='f3 link dim black underlinepa3 pointer'>Sign Out</p>
 		</nav>
 	);
 }

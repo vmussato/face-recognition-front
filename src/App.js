b0f27f40f1bc9501b09dd19c82c776d3eb614530
Vinicius Mussato <vinicius.mussato@gmail.com>
2018-01-27 14:01:38 -0200

    Sign in is integrated with the API

--- src/App.js
+++ src/App.js
@@ -37,6 +37,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(console.log)
+  }
+
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }

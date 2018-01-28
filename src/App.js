6b5e1e0fef51ce526b696e2607d94ca75780d56c
Vinicius Mussato <vinicius.mussato@gmail.com>
2018-01-28 00:11:22 -0200

    Added a initial state to avoid former user trash

--- src/App.js
+++ src/App.js
@@ -25,23 +25,25 @@ const particlesOptions = {
       }
     }
   }
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route:'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route:'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -94,6 +96,7 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count}))
         })
+        .catch(console.log)
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
@@ -102,7 +105,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
